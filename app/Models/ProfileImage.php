97c89b0eda632d9925e832a4de5b4624bcb02636
boost <boost@boosts-MacBook-Air.local>
2023-10-29 00:12:01 +0700

    add profile-image

---
+++ app/Models/ProfileImage.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\UserInfo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProfileImage extends Model
+{
+    use HasFactory;
+
+    public function user_info() {
+        return $this->belongsTo(UserInfo::class);
+    }
+}
--- app/Models/UserInfo.php
+++ app/Models/UserInfo.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\ProfileImage;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class UserInfo extends Model
 {
@@ -13,5 +15,9 @@ class UserInfo extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function profile_images(): HasMany {
+        return $this->hasMany(ProfileImage::class);
     }
 }
